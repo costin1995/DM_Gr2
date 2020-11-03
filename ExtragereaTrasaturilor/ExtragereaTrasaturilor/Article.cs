89403edbbaca2a0702e6e85eed96b79f9888d205
Alina <Alina@DESKTOP-2PKTKCI>
2020-11-03 18:50:10 +0200

    issue6

---
+++ ExtragereaTrasaturilor/ExtragereaTrasaturilor/Article.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtragereaTrasaturilor
+{
+    class Article
+    {
+        public string title;
+        public string text;
+        List<string> ClassCodes = new List<string>();
+
+        public Article(string t, string tex, List<string> ClassWithCodes)
+        {
+            title = t;
+            text = tex;
+            ClassCodes = ClassWithCodes;
+
+        }
+
+
+    }
+}
